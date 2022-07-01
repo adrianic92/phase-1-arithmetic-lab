19ae5587bcd6ed74e7b839984bf1b0257f4d19d5
Adrian <adrianic92@yahoo.com>
2022-07-01 16:39:53 -0700

    commit tree c816f77fb09b8f6e8979deefbe90a387ca8c0455

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let multiply;
+let num1 = 31;
+let num2 = 2;
+let num3 = 10;
+let num4 = 6;
+let randomAny;
+let random;
+let max;
+let mod;
+
+multiply = num1 * num2;
+random = Math.floor(Math.random() * 100) + 1;
+mod = num3 % num4;
+max = Math.max(1, 2, 4, 6, 20);
